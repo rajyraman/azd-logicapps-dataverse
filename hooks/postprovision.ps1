60a6d827503e8e6d6817603fa235afbbcba494af
Natraj Yegnaraman <natraj@dreamingincrm.com>
2023-06-24 22:40:30 +0000

    Update hooks script

--- hooks/postprovision.ps1
+++ hooks/postprovision.ps1
@@ -1,6 +1,9 @@
-$env = azd env get-values;
-$config = $env.Split("`r`n");
-$applicationId = ($config | Where-Object { $_ -like "DATAVERSE_APPLICATION_ID*" }).Split("=")[1].Replace("""", "");
-$dataverseUrl = ($config | Where-Object { $_ -like "DATAVERSE_URL*" }).Split("=")[1].Replace("""", "");
-$appName = az ad sp list --filter "appId eq '$($applicationId)'" --query '[].displayName' --output tsv;
-Write-Host "`n‼ Confirm that Application ""$appName"" with AppId ""$applicationId"" has the right role in $dataverseUrl ‼" -ForegroundColor Yellow;
+#Credit: https://github.com/Azure/azure-dev/issues/1697#issue-1617610507
+$output = azd env get-values
+foreach ($line in $output) {
+  $name, $value = $line.Split("=")
+  $value = $value -replace '^\"|\"$'
+  [Environment]::SetEnvironmentVariable($name, $value)
+}
+$appName = az ad sp list --filter "appId eq '$env:DATAVERSE_APPLICATION_ID'" --query '[].displayName' --output tsv;
+Write-Host "`n‼ Confirm that Application ""$appName"" with AppId ""$env:DATAVERSE_APPLICATION_ID"" has the right role in $env:DATAVERSE_URL ‼" -ForegroundColor Yellow;
