8ee5d3db08cf36429cafd5c7d5426ae19d163bae
Natraj Yegnaraman <natraj@dreamingincrm.com>
2023-08-08 04:48:34 +1000

    Removing azd env get-values as per https://github.com/Azure/azure-dev/discussions/1613#discussioncomment-6661429

--- hooks/postprovision.ps1
+++ hooks/postprovision.ps1
@@ -1,9 +1,2 @@
-#Credit: https://github.com/Azure/azure-dev/issues/1697#issue-1617610507
-$output = azd env get-values
-foreach ($line in $output) {
-  $name, $value = $line.Split("=")
-  $value = $value -replace '^\"|\"$'
-  [Environment]::SetEnvironmentVariable($name, $value)
-}
 $appName = az ad sp list --filter "appId eq '$env:DATAVERSE_APPLICATION_ID'" --query '[].displayName' --output tsv;
 Write-Host "`n‼ Confirm that Application ""$appName"" with AppId ""$env:DATAVERSE_APPLICATION_ID"" has the right role in $env:DATAVERSE_URL ‼" -ForegroundColor Yellow;
